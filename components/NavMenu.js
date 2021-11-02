f3ed32d366501a763a966e01e289f20cfda159a4
Alice <43660942+XinyuAlice@users.noreply.github.com>
2021-11-01 23:42:59 -0300

    update information for website

---
+++ components/NavMenu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function NavMenu() {
+  return (
+    <div>
+      <ul>
+
+      </ul>
+    </div>
+  )
+}
---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>This is the Project page</h1>
+    </div>
+  );
+}
